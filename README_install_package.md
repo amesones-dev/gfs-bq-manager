c8d9363caad46fab6e640079152c304320be1961
amesones-dev <137169958+amesones-dev@users.noreply.github.com>
2023-08-20 12:28:45 +0100

    Initial commit

---
+++ README_install_package.md
@@ -0,0 +1,11 @@
+# gfs_bq_manager
+### Available at [TestPyPI](https://test.pypi.org)  
+[gfs_bq_manager](https://test.pypi.org/project/gfs-bq-manager/)
+
+### Install package
+```console
+pip install -i https://test.pypi.org/simple/gfs-bq-manager
+python
+	from gbq_manager import GBQManager
+	gl = GBQManager()
+```
---
+++ tests/config/__init__.py
@@ -0,0 +1,13 @@
+# Google BigQuery service account key json file
+import os
+
+
+class TestConfig(object):
+    settings = ['BQ_SA_KEY_JSON_FILE']
+    BQ_SA_KEY_JSON_FILE = os.environ.get('TEST_BQ_SA_KEY_JSON_FILE') or '/etc/secrets/sa_key_bq_tests.json'
+
+    def to_dict(self):
+        r = {}
+        for k in self.settings:
+            r[k] = self.__getattribute__(k)
+        return r
